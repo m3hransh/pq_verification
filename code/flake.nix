35c43bffda4270ef354682aca18f1e0611600c5c
Mehran Shahidi <m.mehrashahidi@gmail.com>
2025-06-30 20:10:43 +0200

    ✨update packages

--- code/flake.nix
+++ code/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "LiquidHaskell Project";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs }:
     let
