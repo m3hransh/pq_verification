0576ef60699a7b392fead08ba106ac064feb74c2
Mehran Shahidi <m.mehrashahidi@gmail.com>
2025-10-26 21:56:15 +0100

    ✨BinomialHeap till merge verified

--- code/src/PriorityQueue/BionomialHeap.hs
+++ code/src/PriorityQueue/BionomialHeap.hs
@@ -1,12 +1,16 @@
 module PriorityQueue.BionomialHeap where
 
+import Control.Arrow (Arrow (second))
 import Language.Haskell.Liquid.ProofCombinators
-import Language.Haskell.Liquid.ProofCombinators (Proof, withProof)
-import PriorityQueue.LeftistHeap (lemma_merge_case1)
-import Prelude hiding (sum)
+import Prelude hiding (max, sum)
 
 {-@ LIQUID "--reflection" @-}
 {-@ LIQUID "--ple" @-}
+
+{-@ reflect max @-}
+{-@ max:: forall <p :: Int -> Bool> . Int <p> -> Int<p> -> Int<p> @-}
+max :: Int -> Int -> Int
+max x y = if x > y then x else y
 
 -- Binary tree with heap property
 {-@ reflect isLowerBound @-}
@@ -15,8 +19,32 @@ isLowerBound :: (Ord a) => a -> BinTree a -> Bool
 isLowerBound _ Empty = True
 isLowerBound v (Bin x l r _) = v <= x && isLowerBound v l && isLowerBound v r
 
-data BinTree a = Empty | Bin {value :: a, left :: BinTree a, right :: BinTree a, height :: Int}
-  deriving (Show)
+{-@ lemma_isLowerBound_transitive :: x:a -> y:{a | x <= y} -> t:{BinTree a | isLowerBound y t} -> {isLowerBound x t} @-}
+lemma_isLowerBound_transitive :: (Ord a) => a -> a -> BinTree a -> Proof
+lemma_isLowerBound_transitive x y Empty = ()
+lemma_isLowerBound_transitive x y (Bin z l r _) =
+  lemma_isLowerBound_transitive x y l
+    &&& lemma_isLowerBound_transitive x y r
+
+{-@ type BinTreeHeight a H = { t : BinTree a | bheight t == H } @-}
+
+{-@ type BinTreeBound a X = {b : BinTree a | isLowerBound X b} @-}
+{-@ data BinTree a = Empty
+      | Bin { value :: a
+          ,left  :: BinTreeBound a value
+          ,right :: BinTreeHeight a (bheight left)
+          ,height :: {h : Nat | h == 1 + bheight right}
+           }
+  @-}
+data BinTree a
+  = Empty
+  | Bin
+      { value :: a
+      , left :: BinTree a
+      , right :: BinTree a
+      , height :: Int
+      }
+  deriving (Show, Eq)
 
 {-@ measure bheight @-}
 {-@ bheight :: BinTree a -> Nat @-}
@@ -24,134 +52,148 @@ bheight :: BinTree a -> Int
 bheight Empty = 0
 bheight (Bin _ _ _ h) = h
 
-{-@ data BinTree a = Empty
-      | Bin { value :: a
-           , left  :: {t : BinTree a | isLowerBound value t}
-          , right :: {t : BinTree a | bheight t == bheight left}
-          , height :: {h : Nat | bheight left + 1 == h && bheight right + 1 == h}
+b :: BinTree Int
+b = Bin 2 (Bin 3 Empty Empty 1) (Bin 4 Empty Empty 1) 2
 
+{-@ data Pennant a = P { root :: a
+           ,size  :: Nat
+           ,bin   :: {b : BinTreeBound a root | bheight b == size}
            }
   @-}
--- Priority queue data structure
-{-@ data ToppedTree a = EmptyTree
-                      | WinnerTree { min :: a
-                           , rest :: {rst : BinTree a | isLowerBound min rst}
-                           }
-  @-}
-data ToppedTree a
-  = EmptyTree
-  | WinnerTree {min :: a, rest :: (BinTree a)}
-  deriving (Show)
+data Pennant a = P {root :: a, size :: Int, bin :: (BinTree a)}
+  deriving (Show, Eq)
 
-{-@ measure tsize @-}
-{-@ tsize :: ToppedTree a -> Nat @-}
-tsize :: ToppedTree a -> Int
-tsize EmptyTree = 0
-tsize (WinnerTree _ t) = 1 + bheight t
+pennantExample :: Pennant Int
+pennantExample = P 3 2 (Bin 4 (Bin 7 Empty Empty 1) (Bin 3 Empty Empty 1) 2)
 
-{-@ singleton :: Ord a => a -> ToppedTree a @-}
-singleton :: (Ord a) => a -> ToppedTree a
-singleton x = WinnerTree x Empty
+{-@ measure psize @-}
+{-@ psize :: Pennant a -> Nat @-}
+psize :: Pennant a -> Int
+psize (P _ s _) = s
+
+{-@ singleton :: Ord a => a -> {v:Pennant a | psize v == 0} @-}
+singleton :: (Ord a) => a -> Pennant a
+singleton x = P x 0 Empty
 
 {-@ measure isEmpty @-}
-{-@ isEmpty :: ToppedTree a -> Bool @-}
-isEmpty :: ToppedTree a -> Bool
-isEmpty EmptyTree = True
+{-@ isEmpty :: Pennant a -> Bool @-}
+isEmpty :: Pennant a -> Bool
 isEmpty _ = False
 
 {-@reflect merge @-}
-{-@ merge :: Ord a => t1: {t: ToppedTree a | tsize t> 0} -> t2: {t : ToppedTree a | tsize t == tsize t1} -> {v: ToppedTree a | tsize v == tsize t1 + 1} @-}
-merge :: (Ord a) => ToppedTree a -> ToppedTree a -> ToppedTree a
-merge (WinnerTree x1 t1) (WinnerTree x2 t2)
-  | x1 <= x2 = WinnerTree x1 ((Bin x2 t2 t1 (bheight t1 + 1)) `withProof` lemma_isLowerBound_transitive x1 x2 t2)
-  | otherwise = WinnerTree x2 ((Bin x1 t1 t2 (bheight t2 + 1)) `withProof` lemma_isLowerBound_transitive x2 x1 t1)
-
-{-@ lemma_isLowerBound_transitive :: x1 : a -> x2 : {v : a | x1 <= v} ->  t : {v : BinTree a | isLowerBound x2 t } -> {isLowerBound x1 t} @-}
-lemma_isLowerBound_transitive :: a -> a -> BinTree a -> Proof
-lemma_isLowerBound_transitive x1 x2 Empty = ()
-lemma_isLowerBound_transitive x1 x2 (Bin x t1 t2 _) = lemma_isLowerBound_transitive x1 x2 t1 &&& lemma_isLowerBound_transitive x1 x2 t2 *** QED
-
--- {-@ splitMin :: Ord a => ToppedTree a -> MinView a @-}
--- splitMin :: (Ord a) => ToppedTree a -> MinView a
--- splitMin EmptyTree = EmptyView
--- splitMin (WinnerTree x bt) = Min x (secondMin bt)
---  where
---   {-@ secondMin :: Ord a => BinTree a -> ToppedTree a @-}
---   secondMin :: (Ord a) => BinTree a -> ToppedTree a
---   secondMin Empty = EmptyTree
---   secondMin (Bin y l r _) = merge (WinnerTree y l) (secondMin r)
---
--- {-@ insert :: Ord a => a -> ToppedTree a -> ToppedTree a @-}
--- insert :: (Ord a) => a -> ToppedTree a -> ToppedTree a
--- insert x pq = merge (singleton x) pq
-
--- Helper function for creating singleton trees
-{-@ singletonTree :: Ord a => x:a -> ToppedTree a @-}
-singletonTree :: (Ord a) => a -> ToppedTree a
-singletonTree x = (WinnerTree x (Empty `withProof` (isLowerBound x Empty)))
-
--- {-@ type BinBinomialHeap a = [ToppedTree a] <{\hd v -> (tsize hd) < (tsize v) }> @-}
--- type BinBinomialHeap a = [ToppedTree a]
---
--- This is errornous because it does not satisfy the invariant that the size of the first tree is less than the second.
--- I think this is error in Liquid Haskell.
--- btree :: BinBinomialHeap Int
--- btree = [WinnerTree 1 (Bin 3 Empty Empty), EmptyTree]
+{-@ merge :: Ord a => t1: Pennant a -> t2: {t : Pennant a | psize t == psize t1} -> {v: Pennant a| psize v == psize t1 + 1 } @-}
+merge :: (Ord a) => Pennant a -> Pennant a -> Pennant a
+merge (P x1 s1 t1) (P x2 s2 t2)
+  | x1 <= x2 = (P x1 (s1 + 1) ((Bin x2 t2 t1 (bheight t1 + 1)) `withProof` (lemma_isLowerBound_transitive x1 x2 t2)))
+  | otherwise = (P x2 (s1 + 1) ((Bin x1 t1 t2 (s1 + 1)) `withProof` lemma_isLowerBound_transitive x2 x1 t1))
 
 {-@ data PList a < p :: a -> a -> Bool> =
         Nil
       | Cons { phd :: a, ptl :: PList < p >  a < p phd > }  @-}
 data PList a = Nil | Cons a (PList a)
 
-{-@ measure bhsize @-}
-{-@ bhsize :: PList a -> Nat @-}
-bhsize :: PList a -> Int
-bhsize Nil = 0
-bhsize (Cons h t) = 1 + bhsize t
-
-{-@ type BionomialHeap a = PList <{\hd v -> (tsize hd) <= (tsize v) }> (ToppedTree a) @-}
-type BionomialHeap a = PList (ToppedTree a)
-
--- Helper functions for BionomialHeap operations
-
-{-@ emptyHeap :: BionomialHeap a @-}
-emptyHeap :: BionomialHeap a
-emptyHeap = Nil
-
-{-@ isEmptyHeap :: BionomialHeap a -> Bool @-}
-isEmptyHeap :: BionomialHeap a -> Bool
-isEmptyHeap Nil = True
-isEmptyHeap _ = False
-
--- Insert a tree into a heap, maintaining size ordering
-{-@ reflect insertTree @-}
-{-@ insertTree :: Ord a => {t : ToppedTree a | tsize t > 0}  -> h : BionomialHeap a -> BionomialHeap a  / [bhsize h] @-}
-insertTree :: (Ord a) => ToppedTree a -> BionomialHeap a -> BionomialHeap a
-insertTree t Nil = Cons t Nil
-insertTree t (Cons h hs)
-  | tsize t < tsize h = Cons t (Cons h hs)
-  | tsize t == tsize h = insertTree (merge t h) hs
-  | otherwise = Cons h (insertTree t hs)
-
---
--- -- Merge two binomial heaps with termination measure
--- {-@ mergeHeaps :: Ord a => h1:BionomialHeap a -> h2:BionomialHeap a -> BionomialHeap a / [len h1 + len h2] @-}
--- mergeHeaps :: (Ord a) => BionomialHeap a -> BionomialHeap a -> BionomialHeap a
--- mergeHeaps Nil h2 = h2
--- mergeHeaps h1 Nil = h1
--- mergeHeaps (Cons t1 h1) (Cons t2 h2)
---   | tsize t1 < tsize t2 = Cons t1 (mergeHeaps h1 (Cons t2 h2))
---   | tsize t1 > tsize t2 = Cons t2 (mergeHeaps (Cons t1 h1) h2)
---   | otherwise = insertTree (merge t1 t2) (mergeHeaps h1 h2)
---
--- -- Insert a single element into a binomial heap
--- {-@ insertHeap :: Ord a => a -> BionomialHeap a -> BionomialHeap a @-}
--- insertHeap :: (Ord a) => a -> BionomialHeap a -> BionomialHeap a
--- insertHeap x h = insertTree (singleton x) h
-
--- Helper function to calculate length of PList
 {-@ measure len @-}
 {-@ len :: PList a -> Nat @-}
 len :: PList a -> Int
 len Nil = 0
-len (Cons _ xs) = 1 + len xs
+len (Cons _ t) = 1 + len t
+
+{-@ data BinomialBit a =
+        Zero { zorder :: Nat }
+      | One  {  oorder :: Nat, pennant :: {p : Pennant a | psize p == oorder}}  @-}
+data BinomialBit a = Zero Int | One Int (Pennant a)
+  deriving (Show, Eq)
+
+binTree :: BinomialBit Int
+binTree = One 2 (P 3 2 (Bin 4 (Bin 7 Empty Empty 1) (Bin 3 Empty Empty 1) 2))
+
+{-@ measure rank @-}
+{-@ rank :: BinomialBit a -> Nat @-}
+rank :: BinomialBit a -> Int
+rank (Zero r) = r
+rank (One r _) = r
+
+{-@ type BinomialHeap a = PList <{\hd v -> (rank hd) == (rank v) + 1 }> (BinomialBit a) @-}
+type BinomialHeap a = PList (BinomialBit a)
+
+{-@ reflect isNil @-}
+{-@ isNil :: BinomialHeap a ->  Bool @-}
+isNil :: BinomialHeap a -> Bool
+isNil Nil = True
+isNil _ = False
+
+{-@ reflect bRank @-}
+{-@  bRank :: BinomialHeap a -> Nat @-}
+bRank :: BinomialHeap a -> Int
+bRank Nil = 0
+bRank (Cons b bs) = rank b
+
+{-@ bheap:: BinomialHeap Int @-}
+bheap :: BinomialHeap Int
+bheap = Cons (Zero 1) (Cons (One 0 (singleton 1)) Nil)
+
+{-@ bSum:: b1: BinomialBit a -> b2: { b: BinomialBit a | rank b == rank b1} -> {b: BinomialBit a | rank b == rank b1}  @-}
+bSum :: (Ord a) => BinomialBit a -> BinomialBit a -> BinomialBit a
+bSum (Zero r1) (Zero r2) = Zero r1
+bSum (Zero r1) (One _ p2) = One r1 p2
+bSum (One _ p1) (Zero r2) = One r2 p1
+bSum (One r1 _) (One _ _) = Zero r1
+
+{-@ bHalfAdder :: b1: BinomialBit a
+               -> b2: {b: BinomialBit a | rank b == rank b1}
+               -> (BinomialBit a, BinomialBit a)<{\s c -> rank s == rank b1 && rank c == rank b1 + 1}> @-}
+bHalfAdder :: (Ord a) => BinomialBit a -> BinomialBit a -> (BinomialBit a, BinomialBit a)
+bHalfAdder b1 b2 = (bSum b1 b2, bCarry b1 b2)
+
+{-@ bFullAdder :: b1: BinomialBit a
+               -> b2: {b: BinomialBit a | rank b == rank b1}
+               -> c: {b: BinomialBit a | rank b == rank b1}
+               -> (BinomialBit a, BinomialBit a)<{\s co -> rank s == rank b1 && rank co == rank b1 + 1}> @-}
+bFullAdder :: (Ord a) => BinomialBit a -> BinomialBit a -> BinomialBit a -> (BinomialBit a, BinomialBit a)
+bFullAdder b1 b2 c =
+  let (s, c1) = bHalfAdder b1 b2
+      (s', c2) = bHalfAdder s c
+   in (s', bSum c1 c2)
+
+{-@ bCarry:: Ord a => b1: {b: BinomialBit a | rank b >= 0} -> b2: {b: BinomialBit a | rank b == rank b1} -> {b: BinomialBit a | rank b == rank b1 + 1}  @-}
+bCarry :: (Ord a) => BinomialBit a -> BinomialBit a -> BinomialBit a
+bCarry (Zero r1) (Zero r2) = Zero (r1 + 1)
+bCarry (Zero r1) (One _ p2) = Zero (r1 + 1)
+bCarry (One _ p1) (Zero r2) = Zero (r2 + 1)
+bCarry (One r1 p1) (One _ p2) = One (r1 + 1) (merge p1 p2)
+
+{-@ bAdd :: {h1: BinomialHeap a | bRank h1 == 0} -> {h2: BinomialHeap a | bRank h2 == 0} -> BinomialHeap a @-}
+bAdd :: (Ord a) => BinomialHeap a -> BinomialHeap a -> BinomialHeap a
+bAdd xs ys = addWithCarry xs ys (Zero 0)
+
+-- Helper lemma: if we have Cons x xs, then rank x == bRank (Cons x xs)
+{-@ lemma_cons_rank :: x:BinomialBit a -> xs:BinomialHeap a -> {rank x == bRank (Cons x xs)} @-}
+lemma_cons_rank :: BinomialBit a -> BinomialHeap a -> Proof
+lemma_cons_rank x xs = ()
+
+-- Helper lemma: if bRank h1 == bRank h2 and both are Cons, their heads have equal rank
+{-@ lemma_equal_brank :: h1:{BinomialHeap a | not (isNil h1)}
+                      -> h2:{BinomialHeap a | not (isNil h2) && bRank h2 == bRank h1}
+                      -> {bRank h1 == bRank h2} @-}
+lemma_equal_brank :: BinomialHeap a -> BinomialHeap a -> Proof
+lemma_equal_brank _ _ = ()
+
+{-@ addWithCarry :: h1: BinomialHeap a
+                  -> {h2: BinomialHeap a |  (bRank h2 == bRank h1 || isNil h1) || isNil h2 }
+                  -> carry: { c : BinomialBit a | ((not isNil h1) => rank carry == bRank h1) && ((not isNil h2) => rank carry == bRank h2)}
+                  -> {b: BinomialHeap a | ((not isNil h1) => rank carry == bRank h1) && ((not isNil h2) => rank carry == bRank h2)} / [len h1, len h2]@-}
+addWithCarry :: (Ord a) => BinomialHeap a -> BinomialHeap a -> BinomialBit a -> BinomialHeap a
+addWithCarry Nil Nil c
+  | c == (Zero 0) = Nil
+  | otherwise = Cons c Nil
+addWithCarry (Cons x xs) Nil (Zero r) = Cons x xs
+addWithCarry (Cons x xs) Nil c@(One r _) =
+  let (s, c') = bFullAdder x (Zero (rank x)) c
+   in Cons s (addWithCarry xs Nil c')
+addWithCarry Nil (Cons y ys) c =
+  let z = Zero (rank y)
+      (s, c') = bFullAdder z y c
+   in Cons s (addWithCarry Nil ys c')
+addWithCarry (Cons x xs) (Cons y ys) c =
+  let (s, c') = bFullAdder x y c
+   in Cons s (addWithCarry xs ys c')
